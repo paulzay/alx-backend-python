4654b7cffe46fc652c5ad3b354dbc9d5d3a9df96
Paul Ogolla <paulotieno2@gmail.com>
2024-08-16 00:06:37 +0300

    6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""function"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """return sum of mixed list"""
+    return sum(mxd_lst)
