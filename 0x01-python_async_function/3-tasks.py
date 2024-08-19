9af454b008f50893a3b0f1e55680a1854aa12410
Paul Ogolla <paulotieno2@gmail.com>
2024-08-19 23:51:01 +0300

    3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,6 +5,6 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random() -> asyncio.Task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """docs"""
-    return asyncio.create_task(wait_random())
+    return asyncio.create_task(wait_random(max_delay))
