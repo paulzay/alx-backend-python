0d82f557f8b18cfeb3965eaf40adcd256a764edb
Paul Ogolla <paulotieno2@gmail.com>
2024-08-20 23:03:27 +0300

    document

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""comment"""
+"""4 tas"""
 
 import asyncio
 from typing import List
@@ -8,7 +8,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def task_wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
-    """doc"""
+    """wait n"""
     delays = []
     for i in range(n):
         delays.append(asyncio.create_task(wait_random(max_delay)))
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -10,6 +10,6 @@ async_comprenhension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """docs"""
+    """measure runtime"""
     await asyncio.gather(*(async_comprenhension() for i in range(4)))
     return time.time() - start_time
