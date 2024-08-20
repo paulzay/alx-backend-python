b4024e854598bd4b81d0d67d38fd853a4913df27
Paul Ogolla <paulotieno2@gmail.com>
2024-08-20 22:49:44 +0300

    0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """async_generator"""
 import asyncio
-from typing import List
+from typing import Generator
 import random
 
 
-async def async_generator() -> List[float]:
+async def async_generator() -> Generator[float, None, None]:
     """async_generator"""
     for _ in range(10):
         await asyncio.sleep(1)
